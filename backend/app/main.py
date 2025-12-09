b9f8aed4ee0d5b99300938f108db0c2c0243fcec
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-12-09 04:38:53 +0000

    connect client side for consuming data

--- backend/app/main.py
+++ backend/app/main.py
@@ -58,7 +58,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["https://stack-nstay.vercel.app"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
